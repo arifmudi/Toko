5e61b1897ec2f6cd99054de2629060914a972383
cakra <arif.mudi.priyatno@gmail.com>
2020-10-13 01:34:00 +0700

    update env

--- config/toko.php
+++ config/toko.php
@@ -6,5 +6,11 @@ return [
             'api_key' => env('RAJAONGKIR_KEY', null),
             'origin' => env('RAJAONGKIR_ORIGIN', null),
         ],
+        'midtrans' => [
+            'endpoint' => env('MIDTRANS_endpoint', 'https://app.sandbox.midtrans.com/snap/v1/'),
+            'client_key' => env('MIDTRANS_Client_Key', null),
+            'server_key' => env('MIDTRANS_Server_Key', null),
+            'merchant_id' => env('MIDTRANS_ID_Merchant', null),
+        ]
     ],
 ];
